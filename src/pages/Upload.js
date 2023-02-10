cedac5039d7688a28ee72c0e1e0c5632b0a97460
Divyanatha_Dantu <divyanatha.dantu@gmail.com>
2023-02-09 23:29:34 -0500

    initial commit

---
+++ src/pages/Upload.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Upload.css";
+
+function Upload() {
+  return (
+    <div className="upload">
+      <iframe src="http://localhost:8501/upload"></iframe>
+    </div>
+  );
+}
+export default Upload;
