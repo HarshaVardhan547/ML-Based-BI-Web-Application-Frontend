ab821597dad86ae11ea7f98d1821c74cab48462f
Divyanatha_Dantu <divyanatha.dantu@gmail.com>
2023-02-16 04:54:00 -0500

    Added Email Functionality

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,53 +1,26 @@
 import React from "react";
+import emailjs from "@emailjs/browser";
+
 function ContactUs() {
+  const [yourName, setYourName] = React.useState("");
+  const [yourEmail, setYourEmail] = React.useState("");
+  const [yourPhone, setYourPhone] = React.useState("");
+  const [message, setMessage] = React.useState("");
   function sendEmail() {
-    // let data = {
-    //   personalizations: "",
-    //   from: {
-    //     email: "orders@example.com",
-    //     name: "Example Order Confirmation",
-    //   },
-    //   subject: "Your Example Order Confirmation",
-    //   content: [
-    //     {
-    //       type: "text/html",
-    //       value:
-    //         "<p>Hello from Twilio SendGrid!</p><p>Sending with the email service trusted by developers and marketers for <strong>time-savings</strong>, <strong>scalability</strong>, and <strong>delivery expertise</strong>.</p><p>%open-track%</p>",
-    //     },
-    //   ],
-    // };
-    // axios
-    //   .post("https://api.sendgrid.com/v3/mail/send", data, {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Authorization:
-    //         "Bearer <<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>>",
-    //     },
-    //   })
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-    // sgMail.setApiKey(
-    //   "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"
-    // );
-    // const msg = {
-    //   to: "abhi.capstone1@gmail.com", // Change to your recipient
-    //   from: "abhi.capstone1@gmail.com", // Change to your verified sender
-    //   subject: "Sending with SendGrid is Fun",
-    //   text: "and easy to do anywhere, even with Node.js",
-    //   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-    // };
-    // sgMail
-    //   .send(msg)
-    //   .then(() => {
-    //     console.log("Email sent");
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
+    let templateParams = {
+      from_name: yourName,
+      from_email: yourEmail,
+      from_phone: yourPhone,
+      message: message,
+    };
+    emailjs.send().then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
   }
   return (
     <div className="main" id="about">
@@ -56,10 +29,37 @@ function ContactUs() {
           <div className="left"></div>
           <div className="right">
             <h2>Contact Us</h2>
-            <input type="text" className="field" placeholder="Your Name" />
-            <input type="text" className="field" placeholder="Your Email" />
-            <input type="text" className="field" placeholder="Phone" />
-            <textarea placeholder="Message" className="field"></textarea>
+            <input
+              type="text"
+              className="field"
+              placeholder="Your Name"
+              onChange={(event) => {
+                setYourName(event.target.value);
+              }}
+            />
+            <input
+              type="text"
+              className="field"
+              placeholder="Your Email"
+              onChange={(event) => {
+                setYourEmail(event.target.value);
+              }}
+            />
+            <input
+              type="text"
+              className="field"
+              placeholder="Phone"
+              onChange={(event) => {
+                setYourPhone(event.target.value);
+              }}
+            />
+            <textarea
+              placeholder="Message"
+              className="field"
+              onChange={(event) => {
+                setMessage(event.target.value);
+              }}
+            ></textarea>
             <button className="main__btn" onClick={sendEmail}>
               Send
             </button>
