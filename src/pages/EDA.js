9aa0708bf0331cf503173946ef7dd79d061684eb
Divyanatha_Dantu <divyanatha.dantu@gmail.com>
2023-03-21 20:09:33 -0400

    Fixed Streamlit page layout

--- src/pages/EDA.js
+++ src/pages/EDA.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function EDA() {
   return (
-    <div className="aspect-w-16 aspect-h-16">
+    <div className="aspect-w-16 aspect-h-16 ml-48">
       <iframe src="https://streamlit.metriverse.systems/EDA"></iframe>
     </div>
   );
--- src/pages/Insights.js
+++ src/pages/Insights.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Modeling() {
   return (
-    <div className="aspect-w-16 aspect-h-16">
+    <div className="aspect-w-16 aspect-h-16 ml-48">
       <iframe src="https://streamlit.metriverse.systems/modeling"></iframe>
     </div>
   );
